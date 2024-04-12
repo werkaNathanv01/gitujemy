5ecc3dd4204a97158d9bf7daef2c2831c5a33672
Weronika Kasprzyk <qlairanathan@outlook.com>
2024-04-12 14:01:25 +0200

    Dodanie main

---
+++ main.py
@@ -0,0 +1,12 @@
+class Program:
+    def poczatek(self):
+        tekst_pierwszy = "Uczymy się używać gita"
+        tekst_drugi = "Będziemy expertami w gicie, niedługo"
+        return tekst_pierwszy, tekst_drugi
+
+    def manageRockets(self):
+        pass
+
+    def manage(self):
+        pass
+
